cbc5cbaa578273e92385a41f7e50c1d686eecd27
Vibhu Khurana <kkhurana.vibhu@gmail.com>
2020-08-30 11:06:55 +0530

    fetched real data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Dracula',
-          id: 101,
-        },
-        {
-          name: 'Zombie',
-          id: 102,
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
