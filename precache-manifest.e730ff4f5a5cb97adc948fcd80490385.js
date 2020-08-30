e8c58187f859cf699429cb6137e30bcf9ff68a15
Vibhu Khurana <kkhurana.vibhu@gmail.com>
2020-08-30 16:14:52 +0530

    Updates

---
+++ precache-manifest.e730ff4f5a5cb97adc948fcd80490385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606ed5482bd306461347efb2a31bbfcc",
+    "url": "/React-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec287ac2973801a34753",
+    "url": "/React-monsters-rolodex/static/css/main.d72b79df.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/React-monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec287ac2973801a34753",
+    "url": "/React-monsters-rolodex/static/js/main.dab26a34.chunk.js"
+  },
+  {
+    "revision": "bd378bd2c5ad18e50efa",
+    "url": "/React-monsters-rolodex/static/js/runtime-main.e7b3a27c.js"
+  }
+]);
